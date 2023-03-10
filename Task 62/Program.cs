cc6d4d9c676a7eba69e660e27fd11fd9150a2f91
Raurut <makey@yandex.ru>
2023-03-10 11:14:52 +0300

    Task 62 complete

---
+++ Task 62/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите размер массива");
+int size = int.Parse(ReadLine()!);
+
+int[,] nums = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+
+PrintArray(nums);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine("");
+    }
+}
