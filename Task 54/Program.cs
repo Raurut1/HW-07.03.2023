e235d3c0fd502e0b168512f3197a7e4e956a353c
Raurut <makey@yandex.ru>
2023-03-10 10:59:08 +0300

    Task 54 complete

---
+++ Task 54/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("введите количество строк");
+int linesVol = int.Parse(ReadLine()!);
+WriteLine("введите количество столбцов");
+int columnsVol = int.Parse(ReadLine()!);
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+WriteLine();
+WriteLine("Массив до изменения");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1]) 
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
+    }
+}
+WriteLine();
+WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine("");
+    }
+}
