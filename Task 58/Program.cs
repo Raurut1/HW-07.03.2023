94920be2b65ec590792f14983d72daf1e3dd871e
Raurut <makey@yandex.ru>
2023-03-10 11:06:28 +0300

    Task 58 complete

---
+++ Task 58/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+Clear();
+int InputInt(string output)
+{
+    Write(output);
+    return int.Parse(ReadLine()!);
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 7); 
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        Write("]");
+        WriteLine("");
+    }
+}
+
+int size = InputInt("размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+WriteLine("Матрица - А");
+PrintArray(matrixA);
+WriteLine();
+WriteLine("Матрица - В");
+PrintArray(matrixB);
+WriteLine();
+WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
